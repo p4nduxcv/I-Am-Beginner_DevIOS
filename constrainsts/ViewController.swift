c6f811a04fa7192d2646b71acd4df50e573016ed
Pandula Mandawala <panduxcv@Pandulas-Mac.local>
2019-08-22 12:47:48 +0530

    initial commit

---
+++ constrainsts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  constrainsts
+//
+//  Created by Pandula Mandawala on 8/20/19.
+//  Copyright © 2019 Pandula Mandawala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
